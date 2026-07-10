19c3ac71a069315e1aaa6d656d58d6f83b6d0eb4
김민규 <alsrb0332@naver.com>
2026-07-10 20:27:15 +0900

    feat: 스웨거 설정 추가

---
+++ src/main/java/com/team4/hackerton/global/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.team4.hackerton.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info().title("공모집").description("공모집 서비스 API 문서").version("0.1.0");
+
+        // JWT 토큰 헤더 방식
+        String securityScheme = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+
+        Components components = new Components()
+                .addSecuritySchemes(
+                        securityScheme,
+                        new SecurityScheme()
+                                .name(securityScheme)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("http://localhost:8080").description("로컬"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
